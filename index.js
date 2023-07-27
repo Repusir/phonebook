fea62eafd40237262d5ba3a0e702202856d9a4cb
Adrian Hernandez <repusoir@gmail.com>
2023-07-27 15:54:10 -0600

    [Upload]: Exercise 3.22

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const crypto = require('crypto');
 // 3.7: Add morgan for logging middleware
 const morgan = require('morgan');
+
 const app = express();
 /**
  * CORS are needed in case we are acessing information from another origin
@@ -14,7 +15,7 @@ const app = express();
 const cors = require('cors');
 
 const unknownEndpoint = (_, response) => {
-    response.status(404).send({ error: 'unknown endpoint' });
+  response.status(404).send({ error: 'unknown endpoint' });
 };
 
 /**
@@ -23,85 +24,85 @@ const unknownEndpoint = (_, response) => {
  */
 
 const errorHandlerMiddleWare = function (error, request, response, next) {
-    console.log(error);
+  console.log(error);
 
-    if ( error.name === "CastError" )
-        return response.status(400).end();
-    else if (error.name === 'ValidationError')
-        return response.status(400).json({ error: error.message })
+  if (error.name === 'CastError') return response.status(400).end();
+  if (error.name === 'ValidationError') return response.status(400).json({ error: error.message });
 
-    next(error);
+  next(error);
 };
 
 /**
  * 3.7: Phonebook backend step7
- * - Add the morgan middleware to your application for logging. Configure it to log messages to your console based on the tiny configuration.
+ * - Add the morgan middleware to your application for logging.
+ * Configure it to log messages to your console based on the tiny configuration.
  * 3.8*: Phonebook backend step8
  * - Configure morgan so that it also shows the data sent in HTTP POST requests:
  */
 
-morgan.token('dataSent', function (req) { return JSON.stringify(req.body) });
+morgan.token('dataSent', (req) => JSON.stringify(req.body));
 
 /**
  * MiddleWares
  */
 
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens['dataSent'](req)
-    ].join(' ')
-}));
+app.use(morgan((tokens, req, res) => [
+  tokens.method(req, res),
+  tokens.url(req, res),
+  tokens.status(req, res),
+  tokens.res(req, res, 'content-length'), '-',
+  tokens['response-time'](req, res), 'ms',
+  tokens.dataSent(req),
+].join(' ')));
 
 /**
  * 3.11 phonebook full stack
  * Generate a production build of your frontend, and add it to the internet application using the method introduced in this part.
- * 
+ *
  * NB If you use Render, make sure the directory build is not gitignored
  * Also, make sure that the frontend still works locally (in development mode when started with command npm start).
  * If you have problems getting the app working make sure that your directory structure matches the example app.
  */
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 app.use(express.json());
 
+/**
+ *
 let persons = [
-    { 
+    {
       "id": 1,
-      "name": "Arto Hellas", 
+      "name": "Arto Hellas",
       "number": "040-123456"
     },
-    { 
+    {
       "id": 2,
-      "name": "Ada Lovelace", 
+      "name": "Ada Lovelace",
       "number": "39-44-5323523"
     },
-    { 
+    {
       "id": 3,
-      "name": "Dan Abramov", 
+      "name": "Dan Abramov",
       "number": "12-43-234345"
     },
-    { 
+    {
       "id": 4,
-      "name": "Mary Poppendieck", 
+      "name": "Mary Poppendieck",
       "number": "39-23-6423122"
     }
 ];
+ */
 
 /**
  * 3.13: Phonebook database, step1
- * 
+ *
  * Change the fetching of all phonebook entries so that the data is fetched from the database.
  * Verify that the frontend works after the changes have been made.
- * 
+ *
  * In the following exercises, write all Mongoose-specific code into its own module
  * just like we did in the chapter Database configuration into its own module.
  */
 
-const Persons = require('./models/person')
+const Persons = require('./models/person');
 
 /**
  * Step 3.1
@@ -113,21 +114,18 @@ app.get('/api/persons', (_, response) => {
  */
 
 app.get('/api/persons', (_, response, next) => {
-    Persons
-        .find({})
-        .then(results => {
-            if (results)
-                response.json(results);
-            else
-                response.status(404).end();
-        })
-        .catch(err => next(err));
+  Persons
+    .find({})
+    .then((results) => {
+      if (results) response.json(results);
+      else response.status(404).end();
+    })
+    .catch((err) => next(err));
 });
 
 /**
  * Step 3.2
  * Implement a page at the address http://localhost:3001/info that looks roughly like this:
-
 
  * The page has to show the time that the request was received how many entries are in the phonebook at the time of processing the request.
  *
@@ -137,70 +135,67 @@ app.get('/api/persons', (_, response, next) => {
  */
 
 app.get('/info', (_, response, next) => {
-    Persons
-        .countDocuments({})
-        .then(results => {
-            const res = `
+  Persons
+    .countDocuments({})
+    .then((results) => {
+      const res = `
                 <p>Phonebook has info for ${results} people</br>${new Date()}</p>
             `;
 
-            response.send(res);
-        })
-        .catch(err => next(err));
+      response.send(res);
+    })
+    .catch((err) => next(err));
 });
 
 /**
  * Step 3.3
  * Implement the functionality for displaying the information for a single phonebook entry.
  * The url for getting the data for a person with the id 5 should be http://localhost:3001/api/persons/5
- * 
+ *
  * If an entry for the given id is not found, the server has to respond with the appropriate status code.
  */
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
+  const { id } = request.params;
 
-    Persons
-        .findById(id)
-        .then(results => {
-            if (results)
-                response.json(results);
-            else
-                response.status(404).end();
-        })
-        .catch(err => next(err));
+  Persons
+    .findById(id)
+    .then((results) => {
+      if (results) response.json(results);
+      else response.status(404).end();
+    })
+    .catch((err) => next(err));
 });
 
 /**
  * 3.4: Phonebook backend step4
  * Implement functionality that makes it possible to delete a single phonebook entry by making an HTTP DELETE request to the unique URL of that phonebook entry.
- * 
+ *
  * Test that your functionality works with either Postman or the Visual Studio Code REST client.
- * 
+ *
  * 3.15: Phonebook database, step3
  * Change the backend so that deleting phonebook entries is reflected in the database.
- * 
+ *
  * Verify that the frontend still works after making the changes.
  */
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
+  const { id } = request.params;
 
-    Persons
-        .findByIdAndRemove(id)
-        .then(result => {
-            console.log(result);
-            response.status(204).end();
-        })
-        .catch(err => next(err));
-    
+  Persons
+    .findByIdAndRemove(id)
+    .then((result) => {
+      console.log(result);
+      response.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
 /**
  * Step 3.5
  * Expand the backend so that new phonebook entries can be added by making HTTP POST requests to the address
  * http://localhost:3001/api/persons.
- * 
+ *
  * Generate a new id for the phonebook entry with the Math.random function.
  * Use a big enough range for your random values so that the likelihood of creating duplicate ids is small.
  * https://hackernoon.com/using-javascript-to-create-and-generate-uuids
@@ -209,68 +204,65 @@ app.delete('/api/persons/:id', (request, response, next) => {
  * Implement error handling for creating new entries. The request is not allowed to succeed, if:
  * ~ The name or number is missing
  * ~ The name already exists in the phonebook
- * 
+ *
  * Respond to requests like these with the appropriate status code, and also send back information that explains the reason for the error, e.g.:
- * 
+ *
  * function generateId() {
  * return crypto.randomUUID();
  * }
  */
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body;
-    let res = {
-        name: body.name,
-        number: body.number
-    };
+  const { body } = request;
+  const res = {
+    name: body.name,
+    number: body.number,
+  };
 
-    if (!res.name && !res.number) {
-        const error = !res.name ? 'name' : 'number'
-        return response.status(400).json({ error : `${error} property is missing`});
-    }
+  if (!res.name && !res.number) {
+    const error = !res.name ? 'name' : 'number';
+    return response.status(400).json({ error: `${error} property is missing` });
+  }
 
-    const person = new Persons(res);
+  const person = new Persons(res);
 
-    person
-        .save()
-        .then(result => {
-            response.json(result);
-        })
-        .catch(err => next(err));
+  person
+    .save()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => next(err));
 });
 
 /**
  * 3.17*: Phonebook database, step5
  * If the user tries to create a new phonebook entry for a person whose name is already in the phonebook,
  * the frontend will try to update the phone number of the existing entry by making an HTTP PUT request to the entry's unique URL.
- * 
+ *
  * Modify the backend to support this request.
- * 
+ *
  * Verify that the frontend works after making your changes.
  */
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body;
-    const id = request.params.id;
+  const { body } = request;
+  const { id } = request.params;
 
-    let person = {
-        name: body.name,
-        number: body.number
-    };
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
 
-    Persons
-        .findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            if (updatedPerson)
-                response.status(200).json(updatedPerson);
-            else
-                response.status(404).end();
-        })
-        .catch(err => next(err));
+  Persons
+    .findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+    .then((updatedPerson) => {
+      if (updatedPerson) response.status(200).json(updatedPerson);
+      else response.status(404).end();
+    })
+    .catch((err) => next(err));
 });
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- models/person.js
+++ models/person.js
@@ -6,57 +6,57 @@ mongoose.set('strictQuery', false);
 const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri)
-    .then(_ => {
-        console.log('Connected to MongoDB')
-    })
-    .catch(err => {
-        console.log(`Error while trying to connect to MongoDB through driver uri ${uri}:`, err.message);
-    })
+  .then((_) => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(`Error while trying to connect to MongoDB through driver uri ${uri}:`, err.message);
+  })
 ;
 
 /**
  * 3.19*: Phonebook database, step7
  * Expand the validation so that the name stored in the database has to be at least three characters long.
- * 
+ *
  * 3.20*: Phonebook database, step8
  * Add validation to your phonebook application, which will make sure that phone numbers are of the correct form. A phone number must:
- * 
+ *
  * - have length of 8 or more
  * - be formed of two parts that are separated by -, the first part has two or three numbers and the second part also consists of numbers
- * 
+ *
  * eg. 09-1234556 and 040-22334455 are valid phone numbers
  * eg. 1234556, 1-22334455 and 10-22-334455 are invalid
  */
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: [true, `Name is required`]
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'Name is required'],
+  },
+  number: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /^(\d{2}-\d{6,})|(\d{3}-\d{5,})$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return /^(\d{2}-\d{6,})|(\d{3}-\d{5,})$/.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: [true, `Number is required`]
-    }
+    required: [true, 'Number is required'],
+  },
 });
 
 personSchema.set('toJSON', {
-    transform: (_, returnedDocument) => {
-        returnedDocument.id = returnedDocument._id.toString();
+  transform: (document, returnedDocument) => {
+    returnedDocument.id = returnedDocument._id.toString();
 
-        delete returnedDocument._id;
-        delete returnedDocument.__v;
-    }
+    delete returnedDocument._id;
+    delete returnedDocument.__v;
+  },
 });
 
 /**
- * 
+ *
  * personSchema.statics.deleteById = function(id) {
  * return this.deleteOne({ _id: id });
  * }
